9e25aafc7a1cf2b5ddc5aa4f259c08a05344b01b
allen.xu <24822324@qq.com>
2015-11-25 17:28:25 +0800

    ui base.js

---
+++ public/js/ui/base.js
@@ -0,0 +1,32 @@
+/**
+ * Created by 28652 on 2015/11/25.
+ */
+
+define(function(require){
+    var obj = _.create();
+
+    _.extend(obj.prototype, {
+        init: function(o){
+            var w = this;
+
+            w.el = o && o.el;
+
+            var opt = w.constructor.opt, sup = w.constructor.__super__;
+            while(sup){
+                opt = $.extend({}, sup.constructor.opt, opt);
+                sup = sup.constructor.__super__;
+            }
+
+            w.opt = $.extend(
+                true,
+                {},
+                opt || {},
+                data.opt || {},
+                JSON.parse( (w.el && w.el.attr("opt")) || "{}"),
+                w.el && w.el.data("opt") || {}
+            );
+        }
+    });
+
+    return obj;
+});
