25aca5c5d05043230d6972bc50f44d456728ba65
haiyang5279 <24822324@qq.com>
2015-11-25 21:46:01 +0800

    head

---
+++ public/js/ui/popup.js
@@ -0,0 +1,14 @@
+/**
+ * Created by allenxu on 15-11-25.
+ */
+
+define(function(require){
+    var base = require('base');
+    var obj = _.extend(base);
+
+    _.extend(obj.prototype, {
+
+    });
+
+    return obj;
+});
